e70cf792d7f9547a733fdf713d31710321fa63b4
Kartik Tanksali <kartik.tanksali@gmail.com>
2019-03-18 03:06:18 -0700

    Balancing Paranthesis using Stack

---
+++ Balancing_Parathesis_using_stack.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Mar 17 17:27:45 2019
+
+@author: kartiktanksali
+"""
+
+#Balacing the paranthesis using Stack
+
+paranthesis_dict = {"(":")","[":"]","{":"}"}
+
+def BalanceIt(string):
+    stack = []
+    top=-1
+    for i in string:
+        if i in paranthesis_dict.keys():
+            stack.append(i)
+            top+=1
+        elif i in paranthesis_dict.values():
+            if (len(stack)>0) and (i == paranthesis_dict[stack[top]]):
+                stack.pop()
+                top-=1
+            else:
+                return False
+    
+    if len(stack)==0:
+        return True
+
+    
+    
+
+string = input("Enter a expression: ")
+res = BalanceIt(string)
+print("Balanced" if res==True else "Unbalanced")
