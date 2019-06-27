674fec76cec16c99393bc8dce74f29eac755f0e3
Kartik Tanksali <kartik.tanksali@gmail.com>
2019-06-26 20:42:45 -0700

    Count the number of squares

---
+++ count_the_number_of_squares.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Jun 23 18:03:31 2019
+
+@author: kartiktanksali
+"""
+
+#Find the squares
+
+import math 
+def CountSquares(arr):
+    new_lst = []
+    for i in arr:
+        temp = i.split(" ")
+        start = int(temp[0])
+        end = int(temp[1])
+        
+        new_lst.append((math.floor(math.sqrt(end)) - math.ceil(math.sqrt(start)) + 1)) 
+  
+    return new_lst
+
+
+lst = ["9 25","1 20"]
+
+res = CountSquares(lst)
+print(res)
